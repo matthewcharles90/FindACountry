6a61f4f6db3a2033bc947c55ee1dc1734385dc8a
Matthew Charles <64665136+matthewcharles90@users.noreply.github.com>
2021-04-12 12:56:40 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # FindACountry
-
-Created a country informative page using HTML, CSS, an JavaScript.
-
-Fetched the "Find A Country" API from:  https:;//restcountries.eu/rest/v2/all
-
-By clicking, on the flag you will get information on the desired country that you are curious about.
-
-https://matthewcharles90.github.io/FindACountry/
--- script.js
+++ script.js
@@ -1,51 +1,42 @@
 // const url = "https:;//restcountries.eu/rest/v2/all";
+const countriesList = document.getElementById("countries");
+let countries;
+
+//Event Listeners
+
+countriesList.addEventListener("change", function(event) {
+    // console.log(event.target.vale)
+    displayCountryInfo(event.target.value);
+});
 
 
+fetch("https://restcountries.eu/rest/v2/all")
+.then(res => res.json())
+.then(data => initialize(data))
+.catch(err => console.log("Error:", err));
 
-
-(
-    async () => {
-
-        const appContainer = document.getElementById("app");
-
-        const fetchResponse = await fetch("https://restcountries.eu/rest/v2/all");
-
-        const data = await fetchResponse.json();
-
-        data.slice(0, 25).forEach(
-            country => {
-                const { name, capital, flag, region, population } = country;
-                // console.log(name, capital, region, population);
-                const FLAG = `
-                        <div class="flag-div" data-aos="fade-up" data-aos-duration="1200">
-                            <div class="img-div">
-                                <img src=${ flag } alt="${ name }">
-                            </div>
-                            <div class="value-div">
-                                <h3>${ name }</h3>
-                                <p>
-                                    <span>Population</span>
-                                    <span>${ population }</span>
-                                </p>
-                                <p>
-                                    <span>Region</span>
-                                    <span>${ region }</span>
-                                </p>
-                                <p>
-                                    <span>Capital</span>
-                                    <span>${ capital }</span>
-                                </p>
-                            </div>
-                        </div>
-
-                    `;
-                console.log(FLAG);
-                appContainer.innerHTML += FLAG;
-                ;
-
-            }
-        );
-
+function initialize(countriesData) {
+    countries = countriesData;
+    let options = "";
+    //For.Each Method
+    for(let i=0; i<countries.length; i++) {
+        options += `<option value="${countries[i].alpha3Code}">${countries[i].name}</option>`;
     }
+    document.querySelector("#countries").innerHTML = options;
+    displayCountryInfo("AFG");
+}
 
-)();
+function displayCountryInfo(countryByAlpha3Code) {
+    const countryData = countries.find(country => country.alpha3Code === countryByAlpha3Code);
+    console.log(countryData);
+    document.querySelector("#flag-container img").src = countryData.flag;
+    document.querySelector("#flag-container img").alt = `Flag of $(countryData.name)`;
+    document.getElementById("capital").innerHTML = countryData.capital;
+    document.getElementById("dialog-code").innerHTML = `+${countryData.callingCodes[0]}`;
+    document.getElementById("population").innerHTML = countryData.population.toLocaleString("en-US");
+    document.getElementById("currencies").innerHTML = countryData.currencies.map(c => `${c.name} (${c.code})`).join(", ");
+    document.getElementById("region").innerHTML = countryData.region;
+    document.getElementById("subregion").innerHTML = countryData.subregion;
+    
+    
+}
